45cbbbbc0dca4824b719ddc99963efc1b056f6d9
Caique <>
2023-12-05 01:06:50 -0300

    moving detailscreen

--- app/src/main/java/com/caique/aetnatestflickr/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/caique/aetnatestflickr/ui/navigation/AppNavHost.kt
@@ -1,19 +1,11 @@
 package com.caique.aetnatestflickr.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import com.caique.aetnatestflickr.data.model.PhotoItem
-import com.caique.aetnatestflickr.feature.detail.presentation.DetailScreen
-import com.caique.aetnatestflickr.feature.list.presentation.ListViewModel
+import com.caique.aetnatestflickr.feature.list.presentation.DetailScreen
 import com.caique.aetnatestflickr.feature.list.presentation.PhotoList
 import com.caique.aetnatestflickr.ui.Screen
 import com.caique.aetnatestflickr.ui.getDetailPhoto
