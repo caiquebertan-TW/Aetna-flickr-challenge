33be71a868a9e1fcbd90d0cd8a66065d65c47ee2
Caique <>
2023-12-05 01:06:21 -0300

    moving detailscreen

--- app/src/main/java/com/caique/aetnatestflickr/feature/detail/presentation/DetailViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.caique.aetnatestflickr.feature.detail.presentation
-
-import androidx.lifecycle.ViewModel
-import com.caique.aetnatestflickr.data.domain.FlickrRepository
-
-class DetailViewModel(
-    private val flickrRepository: FlickrRepository
-) : ViewModel() {
-}
--- app/src/main/java/com/caique/aetnatestflickr/feature/list/presentation/DetailScreen.kt
+++ app/src/main/java/com/caique/aetnatestflickr/feature/list/presentation/DetailScreen.kt
@@ -1,10 +1,8 @@
 @file:OptIn(ExperimentalGlideComposeApi::class)
 
-package com.caique.aetnatestflickr.feature.detail.presentation
+package com.caique.aetnatestflickr.feature.list.presentation
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,25 +13,18 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import com.caique.aetnatestflickr.R
 import com.caique.aetnatestflickr.data.model.PhotoItem
-import com.caique.aetnatestflickr.feature.list.presentation.ListViewModel
 import com.caique.aetnatestflickr.ui.design.AppTheme
-import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun DetailScreen(photoItem: PhotoItem) {
